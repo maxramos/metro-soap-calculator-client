a88de4e10efd875e3490597be527a68980809486
max <max@max-msi>
2017-01-08 07:19:46 +0800

    enable message dump

---
+++ src/main/java/ph/maxramos/metro/soap/calculator/listener/StartUpListener.java
@@ -0,0 +1,21 @@
+package ph.maxramos.metro.soap.calculator.listener;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+import com.sun.xml.ws.transport.http.client.HttpTransportPipe;
+
+@WebListener
+public class StartUpListener implements ServletContextListener {
+
+	@Override
+	public void contextInitialized(ServletContextEvent sce) {
+		HttpTransportPipe.dump = true;
+	}
+
+	@Override
+	public void contextDestroyed(ServletContextEvent sce) {
+	}
+
+}
